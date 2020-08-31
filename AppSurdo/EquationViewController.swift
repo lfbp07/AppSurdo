bbead1eb7f9ef3cb34b49b336d2e06bf9df13e47
Luis Felipe <lfbp@cin.ufpe.br>
2020-08-31 11:54:14 -0300

    First commit

---
+++ AppSurdo/EquationViewController.swift
@@ -0,0 +1,14 @@
+//
+//  EquationViewController.swift
+//  AppSurdo
+//
+//  Created by Luis Pereira on 31/08/20.
+//  Copyright © 2020 Luis Pereira. All rights reserved.
+//
+
+import Foundation
+import  UIKit
+
+class EquationViewController : UIViewController{
+    
+}
---
+++ AppSurdo/QuestionViwController.swift
@@ -0,0 +1,15 @@
+//
+//  QuestionViwController.swift
+//  AppSurdo
+//
+//  Created by Luis Pereira on 31/08/20.
+//  Copyright © 2020 Luis Pereira. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class QuestionViewController : UIViewController{
+    
+    
+}
