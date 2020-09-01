df75caa679d6c92c04b410a667f35d88e207552f
Luis Felipe <lfbp@cin.ufpe.br>
2020-09-01 16:32:45 -0300

    mainScene e plane detection

--- AppSurdo/ARViewController.swift
+++ AppSurdo/ARViewController.swift
@@ -14,56 +14,95 @@ import ARKit
 class ARViewController: UIViewController, ARSCNViewDelegate {
     
     @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            
-            // Set the view's delegate
-            sceneView.delegate = self
-            
-            // Show statistics such as fps and timing information
-            sceneView.showsStatistics = true
-            
-            // Create a new scene
-            let scene = SCNScene(named: "art.scnassets/ship.scn")!
-            
-            // Set the scene to the view
-            sceneView.scene = scene
-        }
+        // Set the view's delegate
+        sceneView.delegate = self
         
-        override func viewWillAppear(_ animated: Bool) {
-            super.viewWillAppear(animated)
-            
-            // Create a session configuration
-            let configuration = ARWorldTrackingConfiguration()
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+        
+        // Create a new scene
+        let scene = SCNScene(named: "art.scnassets/mainScene.scn")!
+        
+        // Set the scene to the view
+        sceneView.scene = scene
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
+        UIApplication.shared.isIdleTimerDisabled = true
+        sceneView.autoenablesDefaultLighting = true
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        print("new plane")
+        // Cast ARAnchor as ARPlaneAnchor
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
 
-            // Run the view's session
-            sceneView.session.run(configuration)
-        }
-        
-        override func viewWillDisappear(_ animated: Bool) {
-            super.viewWillDisappear(animated)
-            
-            // Pause the view's session
-            sceneView.session.pause()
-        }
+        // Create SCNGeometry from ARPlaneAnchor details
+        let width = CGFloat(planeAnchor.extent.x)
+        let height = CGFloat(planeAnchor.extent.z)
+        let planeGeometry = SCNPlane(width: width, height: height)
 
-        // MARK: - ARSCNViewDelegate
+        // Add material to geometry
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.blue.withAlphaComponent(0.8)
+        planeGeometry.materials = [material]
+
+        // Create a SCNNode from geometry
+        let planeNode = SCNNode(geometry: planeGeometry)
+        planeNode.position = SCNVector3(planeAnchor.center.x, planeAnchor.center.y, planeAnchor.center.z)
+        planeNode.eulerAngles.x = -.pi / 2 
+
+        // Add the newly created plane node as a child of the node created for the ARAnchor
+        node.addChildNode(planeNode)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        print("update plane")
+        guard let planeAnchor = anchor as? ARPlaneAnchor, let planeNode = node.childNodes.first,
+        let planeGeometry = planeNode.geometry as? SCNPlane else { return }
+
+        // Update the dimensions of the plane geometry based on the plane anchor.
+        planeGeometry.width = CGFloat(planeAnchor.extent.x)
+        planeGeometry.height = CGFloat(planeAnchor.extent.z)
+
+        // Update the position of the plane node based on the plane anchor.
+        planeNode.position = SCNVector3(planeAnchor.center.x, planeAnchor.center.y, planeAnchor.center.z)
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
         
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+    // MARK: - ARSCNViewDelegate
+    
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
         
-        func session(_ session: ARSession, didFailWithError error: Error) {
-            // Present an error message to the user
-            
-        }
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
         
-        func sessionWasInterrupted(_ session: ARSession) {
-            // Inform the user that the session has been interrupted, for example, by presenting an overlay
-            
-        }
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
         
-        func sessionInterruptionEnded(_ session: ARSession) {
-            // Reset tracking and/or remove existing anchors if consistent tracking is required
-            
-        }
+    }
     
 }
