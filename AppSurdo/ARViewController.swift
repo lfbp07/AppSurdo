8a62fe56caf3da7252924114a755b8e627aef487
Luis Felipe <lfbp@cin.ufpe.br>
2020-09-02 20:40:13 -0300

    tap to place scenario

--- AppSurdo/ARViewController.swift
+++ AppSurdo/ARViewController.swift
@@ -14,6 +14,7 @@ import ARKit
 class ARViewController: UIViewController, ARSCNViewDelegate {
     
     @IBOutlet var sceneView: ARSCNView!
+    var baseLoaded:Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,13 +24,37 @@ class ARViewController: UIViewController, ARSCNViewDelegate {
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
+        sceneView.debugOptions = [.showFeaturePoints]
+        
         
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/mainScene.scn")!
+        //let scene = SCNScene(named: "art.scnassets/mainScene.scn")!
         
         // Set the scene to the view
-        sceneView.scene = scene
+        //sceneView.scene = scene
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if baseLoaded == false{
+            print("Load scenario")
+            baseLoaded = true
+            addScenario(location: (touches.first?.location(in: sceneView))!)
+        }else{
+            print("Scenario loaded")
+        }
+    }
+    
+    func addScenario(location:CGPoint){
+        let hitResult = sceneView.hitTest(location, types: [.existingPlaneUsingExtent])
+        if hitResult.count > 0 {
+            let result = hitResult.first!
+            let newPosition = SCNVector3(result.worldTransform.columns.3.x, result.worldTransform.columns.3.y, result.worldTransform.columns.3.z)
+            let scene = SCNScene(named: "art.scnassets/mainScene.scn")!
+            let rootNode = scene.rootNode.childNode(withName: "BaseReference", recursively: false)
+            rootNode?.position = newPosition
+            sceneView.scene = scene
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -44,42 +69,54 @@ class ARViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.run(configuration)
     }
     
-    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+    func addNodeAtLocation(location:CGPoint){
+        print("adding node...")
+    }
+    
+   /* func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        
         print("new plane")
         // Cast ARAnchor as ARPlaneAnchor
         guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
-
+        
         // Create SCNGeometry from ARPlaneAnchor details
         let width = CGFloat(planeAnchor.extent.x)
         let height = CGFloat(planeAnchor.extent.z)
         let planeGeometry = SCNPlane(width: width, height: height)
-
+        
         // Add material to geometry
         let material = SCNMaterial()
         material.diffuse.contents = UIColor.blue.withAlphaComponent(0.8)
         planeGeometry.materials = [material]
-
+        
         // Create a SCNNode from geometry
         let planeNode = SCNNode(geometry: planeGeometry)
         planeNode.position = SCNVector3(planeAnchor.center.x, planeAnchor.center.y, planeAnchor.center.z)
-        planeNode.eulerAngles.x = -.pi / 2 
-
+        planeNode.eulerAngles.x = -.pi / 2
+        
         // Add the newly created plane node as a child of the node created for the ARAnchor
+        
         node.addChildNode(planeNode)
+        
+        
     }
     
     func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
         print("update plane")
         guard let planeAnchor = anchor as? ARPlaneAnchor, let planeNode = node.childNodes.first,
-        let planeGeometry = planeNode.geometry as? SCNPlane else { return }
-
+            let planeGeometry = planeNode.geometry as? SCNPlane else { return }
+        
         // Update the dimensions of the plane geometry based on the plane anchor.
         planeGeometry.width = CGFloat(planeAnchor.extent.x)
         planeGeometry.height = CGFloat(planeAnchor.extent.z)
-
+        
         // Update the position of the plane node based on the plane anchor.
         planeNode.position = SCNVector3(planeAnchor.center.x, planeAnchor.center.y, planeAnchor.center.z)
     }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
+        print("removed")
+    }*/
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
